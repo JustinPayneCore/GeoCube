bdf6f72f073b9a5deeb5ceb282c0c2aa609acb02
Justin Payne <justinthecore@gmail.com>
2024-05-31 13:22:47 -0700

    Added Game Over & Level Complete screens
    added screens and functionality for the Game over and Level Complete screens

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject gameOverScreen;
+    public GameObject levelCompleteScreen;
+
     bool gameHasEnded = false;
 
-    public float restartDelay = 5;
+    public float screenDelay = 2;
+    public float restartDelay = 1;
+    public float levelCompleteDelay = 1;
 
     // Called if the current level is complete successfully.
     public void CompleteLevel()
     {
         Debug.Log("Game Mananger, Level Complete");
+        Invoke("ShowLevelCompleteScreen", levelCompleteDelay);
     }
 
     // Called if the current level is failed.
@@ -20,9 +26,43 @@ public class GameManager : MonoBehaviour
         {
             gameHasEnded = true;
             Debug.Log("Game Mananger, Level Lost");
-            Invoke("Restart", restartDelay);
+            Invoke("ShowGameOverScreen", screenDelay);
         }
 
+    }
+
+    // Starts the next level
+    public void NextLevelButton()
+    {
+
+    }
+
+    // Triggers the restart function with a delay
+    public void RestartButton()
+    {
+        Invoke("Restart", restartDelay);
+    }
+
+    // Closes the application
+    public void QuitButton()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+			Application.Quit();
+        #endif
+    }
+
+    // Sets the Game Over Screen to visible.
+    void ShowGameOverScreen()
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+    // Sets the Level Complete screen to visible.
+    void ShowLevelCompleteScreen()
+    {
+        levelCompleteScreen.SetActive(true);
     }
 
     // Restarts the current level being played.
